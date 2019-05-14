dc46afe18daaf78a72b0b34cecd0d6a3056ecc66
diego dalairac <stereotipoarg@gmail.com>
2019-05-14 17:02:48 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { Page404Component } from './components/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'page404', component: Page404Component },
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '**', redirectTo: '/page404', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { Page404Component } from './components/page404/page404.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent,
+    Page404Component,
+    NavComponent
   ],
   imports: [
     BrowserModule,
